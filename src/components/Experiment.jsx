7c53b5aa716fb2d8481229be72f864a53118c677
mazharulalam26@gmail.com <mazharulalam26@gmail.com>
2024-02-11 21:51:46 +0600

    moila

--- src/components/Experiment.jsx
+++ src/components/Experiment.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 function Experiment() {
-    const [d, setd] = useState()
-    useEffect(() => {
-        // axios.get("https://www.eporner.com/api/v2/video/search/?query=${riley my heart}&per_page=${10}&page=${1}&thumbsize=${thumbsize}&order=${order}&gay=${gay}&lq=${lq}&format=json").then((d) => {
-        axios.get("https://cdn.jsdelivr.net/npm/pornhub-api@0.0.1/src/Videos.min.js").then((d) => {
-            setd(d);
-        })
-        .catch(e => console.log(e))
-
-    })
+  const [d, setd] = useState()
+  useEffect(() => {
+    (function (ctd) {
+      var d = document,
+        s = d.createElement('script'),
+        l = d.scripts[d.scripts.length - 1];
+      s.settings = ctd || {};
+      s.src = "\/\/appropriateanswer.com\/bQXSV.sAdKGTlu0JYyWFd\/isYyW_5\/uZZWXFIR\/Hecm\/9nu\/ZIUjlqkBPzTNUqx-OST\/YwyON\/zdAGtgNpTmEQ5rNFjNIU3YMwQM";
+      s.async = true;
+      l.parentNode.insertBefore(s, l);
+    })({})
+  })
   return (
     <div>Experiment</div>
   )
