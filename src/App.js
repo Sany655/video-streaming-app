12ccac19d6578bca9065a93ca56f0b700cd232e0
mazharulalam26@gmail.com <mazharulalam26@gmail.com>
2024-03-02 00:33:54 +0600

    deployed testing ads

--- src/App.js
+++ src/App.js
@@ -10,12 +10,22 @@ import SavedVideos from "./components/SavedVideos";
 import Contexts from "./Contexts";
 
 const App = () => {
-
+  useEffect(() => {
+    if (process.env.NODE_ENV) {
+      console.log('string','Development mode');
+      console.log('variable',process.env.NODE_ENV);
+    }else{
+      console.log('string','peoduction mode');
+      console.log('variable',process.env.NODE_ENV);
+    }
+  }, [])
+  
   axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
   return (
     <Contexts>
       <Box>
         <Navbar />
+        {process.env.NODE_ENV === 'production' && (<script type="text/javascript" src="//www.topcreativeformat.com/11f5206119ac1884d11f82c6562de90f/invoke.js"></script>)}
         <Container maxWidth="xl" sx={{ paddingY: '20px' }} >
           <Routes>
             <Route path="/" exact element={<Feed />} />
